cbe84e72f2695e6b321f3caa30e3482dae85d40d
Artem Ogolets <deadshotgg@gmail.com>
2021-06-14 17:38:09 +0200

    feat: complete second and third tasks

--- Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-2/MessageDecryptor.swift
@@ -3,6 +3,36 @@ import UIKit
 class MessageDecryptor: NSObject {
     
     func decryptMessage(_ message: String) -> String {
-        return ""
+        var countArray = [String]()
+        var repeatCount = ""
+        var wordArray = [String]()
+        var result = ""
+        
+        for element in message {
+            if element.isNumber {
+                repeatCount = "\(repeatCount)\(element)"
+            } else if element == "[" {
+                if repeatCount == "" {
+                    countArray.append("1")
+                } else {
+                    countArray.append(repeatCount)
+                }
+                repeatCount = ""
+                wordArray.append(result)
+                result = ""
+            } else if element == "]" {
+                var tempString = ""
+                if let counts = Int(countArray.popLast() ?? "1") {
+                    for _ in 0..<counts {
+                        tempString += result
+                    }
+                }
+                result = wordArray.popLast()! + tempString
+            } else if element.isLetter {
+                result += String(element)
+            }
+        }
+        
+        return result
+        }
     }
-}
